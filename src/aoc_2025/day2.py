963f83994850caac151b1aebd7515d784ed3c491
David Newman <david.newman@nanoporetech.com>
2025-12-02 09:18:37 +0000

    day2

---
+++ src/aoc_2025/day2.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+input_text = Path("input_data/day2.txt").read_text().strip().split(",")
+
+
+def part_1(input_text):
+    cnt = 0
+    for line in input_text:
+        start = int(line.split("-")[0])
+        end = int(line.split("-")[1])
+        for i in range(start, end + 1):
+            i = str(i)
+            if i[:len(i)//2] == i[len(i)//2:]:
+                cnt += int(i)
+
+
+    return cnt
+
+
+print("Part 1:", part_1(input_text))
+
+
+def is_valid(i):
+    for st in range(len(i)):
+        for end in range(st+1, len(i)):
+            if not len(i) % (end - st):
+                substr = i[st:end]
+                if substr * int(len(i) / len(substr)) == i:
+                    return True
+
+
+def part_2(input_text):
+    cnt = 0
+    for line in input_text:
+        start = int(line.split("-")[0])
+        end = int(line.split("-")[1])
+        for i in range(start, end + 1):
+            if is_valid(str(i)):
+                cnt += i
+    return cnt
+
+print("Part 2:", part_2(input_text))
