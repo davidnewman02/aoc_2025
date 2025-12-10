8a78b9dd0083b5a56e3469f330921085375f9524
David Newman <david.newman@nanoporetech.com>
2025-12-10 22:39:46 +0000

    day10: pt.2

--- src/aoc_2025/day10.py
+++ src/aoc_2025/day10.py
@@ -3,6 +3,7 @@ import itertools
 from pathlib import Path
 
 import numpy as np
+import pulp
 
 input_text = Path("input_data/day10.txt").read_text().split("\n")
 
@@ -27,22 +28,40 @@ def parse_text(input_text):
 
 
 def check_combos(l, s):
-    for i in range(1, len(s)):
-        for cmb in itertools.combinations(s, i):
+    for presses in range(1, len(s)):
+        for cmb in itertools.combinations(s, presses):
             result = functools.reduce(np.bitwise_xor, cmb)
             if np.array_equal(result, l):
-                return i
+                return presses
     return len(s)
 
 
 def part_1(machines):
-    cnt = 0
-    for l, s, _ in machines:
-        count = check_combos(l, s)
-        cnt += count
-    return cnt
+    return sum([check_combos(l, s) for l, s, _ in machines])
 
+
+def part_2(machines):
+    cnt = 0
+    for _, switches, jolts in machines:
+
+        buttons = np.array(switches, dtype=int)
+        n_switches, n_lights = buttons.shape
+
+        # Set up solver and variables
+        prob = pulp.LpProblem("find_presses", pulp.LpMinimize)
+        x = pulp.LpVariable.dicts("x", range(n_switches), lowBound=0, cat="Integer")
+        prob += pulp.lpSum(x[j] for j in range(n_switches))
+
+        # For each light, the sum of contributions from all switches must match its joltage target.
+        # Add these as target constraints
+        for i in range(n_lights):
+            prob += pulp.lpSum(buttons[j, i] * x[j] for j in range(n_switches)) == jolts[i]
+
+        prob.solve(pulp.PULP_CBC_CMD(msg=False))
+        presses = sum([pulp.value(x[j]) for j in range(n_switches)])
+        cnt += presses
+    return int(cnt)
 
 machines = parse_text(input_text)
-
 print("Part 1:", part_1(machines))
+print("Part 2:", part_2(machines))
